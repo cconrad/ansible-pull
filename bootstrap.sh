d8304e6a121360993e9773661590b03d54c77745
Claus Conrad <claus@nem-hr.dk>
2024-07-29 14:07:10 +0200

    Only try to install prerequisites if missing

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-continue_as_user() {
-    printf "\nPrerequisites installed. Now run:\nansible-pull -U https://github.com/cconrad/ansible-pull.git\n"
-}
-
 if [ "$(id -u)" -ne 0 ]; then
   echo "This script must be run using sudo or as root"
   exit 1
@@ -25,11 +21,36 @@ fi
 
 case $OS in
     arch)
-        # TODO Check if the package(s) are already installed
-        if pacman -Sy --noconfirm ansible-core git ; then continue_as_user ; fi
+        if pacman -Q git >/dev/null 2>/dev/null ;
+        then
+            GIT_INSTALLED=1
+        else
+            if pacman -Sy --noconfirm git ;
+            then
+                GIT_INSTALLED=1
+            fi
+        fi
+
+        if pacman -Q ansible-core >/dev/null 2>/dev/null ;
+        then
+            ANSIBLE_INSTALLED=1
+        else
+            if pacman -Sy --noconfirm ansible-core ;
+            then
+                ANSIBLE_INSTALLED=1
+            fi
+        fi
         ;;
     *)
         echo "Unsupported OS: $OS"
         exit 1
         ;;
 esac
+
+if [ "$GIT_INSTALLED" != 1 ] || [ "$ANSIBLE_INSTALLED" != 1 ];
+then
+    echo "Failed to install git and/or ansible-core"
+    exit 1
+else
+    printf "\nPrerequisites installed. Now run:\nansible-pull -U https://github.com/cconrad/ansible-pull.git\n"
+fi
